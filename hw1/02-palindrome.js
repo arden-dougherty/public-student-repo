b631d73e108565f5c854191821ac46009bdb9b76
Arden Dougherty <asd6@pdx.edu>
2025-10-15 13:24:56 -0700

    hw1 complete

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,33 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+// This function takes an integer and converts it into a string to determine whether it is a palindrome
+const isPalindrome = function isThisNumberAPalindrome(n) {
+  let string = n.toString();
+  let length = string.length;
+  // assume the number is a palindrome
+  let palindrome = true;
+
+  // compare characters from the ends going inward
+  // if there is ever a mismatch, the number is not a palindrome
+  for (let i = 0; i < length / 2; ++i) {
+    if (string[i] !== string[length - 1 - i]) palindrome = false;
+  }
+
+  return palindrome;
+};
+
+// this function updates the response element to reflect whether the user input is a palindrome
+const handleInput = function palindromeEventHandler() {
+  let input = document.querySelector("input");
+  let response = document.getElementById("response");
+
+  if (isPalindrome(input.value)) {
+    response.textContent = "Yes. This is a palindrome!";
+    response.setAttribute("class", "text-dark-green");
+  } else {
+    response.textContent = "No. Try again.";
+    response.setAttribute("class", "text-dark-red");
+  }
+};
+
+elem.addEventListener("input", handleInput);
