a4b0bafcbef0d0718df1b92734b1777ae6816495
Arden Dougherty <asd6@pdx.edu>
2025-10-19 16:08:31 -0700

    hw1 code changes

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,33 +1,37 @@
-const elem = document.querySelector("input");
+const input = document.querySelector("input");
 
 // This function takes an integer and converts it into a string to determine whether it is a palindrome
-const isPalindrome = function isThisNumberAPalindrome(n) {
-  let string = n.toString();
-  let length = string.length;
-  // assume the number is a palindrome
-  let palindrome = true;
-
-  // compare characters from the ends going inward
-  // if there is ever a mismatch, the number is not a palindrome
-  for (let i = 0; i < length / 2; ++i) {
-    if (string[i] !== string[length - 1 - i]) palindrome = false;
-  }
+const isPalindrome = function isThisNumberAPalindrome(num) {
+  let palindrome = false;
+  let reverse = num.split("").reverse().join("");
+  if (reverse == num) palindrome = true;
 
   return palindrome;
 };
 
 // this function updates the response element to reflect whether the user input is a palindrome
 const handleInput = function palindromeEventHandler() {
-  let input = document.querySelector("input");
   let response = document.getElementById("response");
 
-  if (isPalindrome(input.value)) {
+  // no response if input is empty
+  if (input.value === "") {
+    response.textContent = "";
+  }
+
+  // error message if number is negative
+  else if (input.value <= 0) {
+    response.textContent = "Please enter a positive number";
+    response.setAttribute("class", "text-danger");
+  }
+
+  // if input is valid, check if its a palindrome
+  else if (isPalindrome(input.value)) {
     response.textContent = "Yes. This is a palindrome!";
-    response.setAttribute("class", "text-dark-green");
+    response.setAttribute("class", "text-success");
   } else {
     response.textContent = "No. Try again.";
-    response.setAttribute("class", "text-dark-red");
+    response.setAttribute("class", "text-danger");
   }
 };
 
-elem.addEventListener("input", handleInput);
+input.addEventListener("input", handleInput);
