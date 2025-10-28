319c5ab5e7066d65f20ba2ae1afa4752970b9a35
Arden Dougherty <asd6@pdx.edu>
2025-10-28 14:06:40 -0700

    hw2 exercises 3 and 4 complete

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,30 @@
-// Add your code here
+const input = document.getElementById("userInput");
+const paragraph = document.getElementById("paragraph");
+const originalText = paragraph.textContent;
+// split the paragraph text into an array of words.
+// keep punctuation by splitting on an empty string
+// only if that empty string is preceded or followed by punctuation/whitespace
+const originalWords = originalText.split(/(?<=[\s,—])|(?=[\s,—])/g);
 
-input.addEventListener('keydown', handleKeyDown);
+// This function takes a string and highlights all matching words in the paragraph text
+const count = function countMatchingWords(str) {
+  // remove existing highlights
+  paragraph.textContent = originalText;
+  let words = Array.from(originalWords);
+  // if the input is empty, highlight nothing
+  if (str !== "") {
+    words.forEach((word, index) => {
+      if (word === str) {
+        words[index] = `<mark>${word}</mark>`;
+      }
+    });
+    paragraph.innerHTML = words.join("");
+  }
+};
+
+const handleInput = function userInputEventHandler() {
+  const userInput = input.value;
+  count(userInput);
+};
+
+input.addEventListener("input", handleInput);
